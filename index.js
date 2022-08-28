475b430359312be6cbc98423f6eac20df140bddf
cykj40 <102045473+cykj40@users.noreply.github.com>
2022-08-28 10:13:00 -0400

    thought i was done but i still had soem minor bugs to sort out
    had to adjust the line before __dirname in two spots. then i had to downgrade my inquirer package because th enew version requires a new syntax not the require("inquirer"); i should have probably taken tge time to do it the new way. i actually probably should have kept the const { default: inquirer } = eqruire("inquirer").

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const { default: inquirer } = require("inquirer");
 
-const OUTPUT_DIR = path.resolve(_dirname, "output");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = ("")
+const render = require("./lib/htmlRenderer");
 
 const teamMembers = [];
 
